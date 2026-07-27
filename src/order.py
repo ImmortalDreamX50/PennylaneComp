98722e777705512d555665e618eb39abceb21893
ImmortalDreamX50 <112034500+ImmortalDreamX50@users.noreply.github.com>
2026-07-27 13:35:00 +0200

    Update and rename order.py to src/order.py

--- src/order.py
+++ src/order.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pennylane as qml
+import pennylane as qp
 
 u5 = np.zeros([32, 32], dtype = int) 
 
@@ -10,7 +10,7 @@ for i in range(21,32):
     
 def CU5(wires):
     """Controlled-U5: wires[0] is control, wires[1:] are target qubits."""
-    qml.ctrl(qml.QubitUnitary(u5, wires=wires[1:]), control=wires[0])
+    qp.ctrl(qp.QubitUnitary(u5, wires=wires[1:]), control=wires[0])
 
 u8 = np.zeros([16, 16], dtype = int) 
 
@@ -21,4 +21,4 @@ for i in range(15,16):
     
 def CU8(wires):
     """Controlled-U8: wires[0] is control, wires[1:] are target qubits."""
-    qml.ctrl(qml.QubitUnitary(u8, wires=wires[1:]), control=wires[0])
+    qp.ctrl(qp.QubitUnitary(u8, wires=wires[1:]), control=wires[0])
